efa97f6a82d202c9b745c0207fc3352ac457a737
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-03-03 13:50:21 -0500

    Fin gestion des déplacements horizontaux de l'avatar

--- Assets/Scripts/GestionAvatar.cs
+++ Assets/Scripts/GestionAvatar.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class GestionAvatar : MonoBehaviour
 {
-    float vitesseX; // Variable de type float qui enregistre la vitesse horizontale de l'avatar
-    public float vitesseXMax; // Variable de type float qui enregistre la vitesse horizontale maximale de l'avatar
+    [Header("Vitesses")]
+    float vitesseX; // Variable de type float qui enregistre la vitesse HORIZONTALE de l'avatar
+    public float vitesseXMax; // Variable de type float qui enregistre la vitesse HORIZONTALE MAXIMALE de l'avatar
+    float vitesseY; // Variable de type float qui enregistre la vitesse VERTICALE de l'avatar
+    public float vitesseSaut; // Variable qui enregistre la vitesse de SAUT de l'avatar
 
-    private static bool avatarEstMort; // Variable statique booléenne qui enregistre si l'avatar est mort
+    public static bool avatarEstMort; // Variable statique booléenne qui enregistre si l'avatar est mort
+    public static bool partieEnCours; // Variable statique booléenne qui enregistre si la partie est en cours //////////////////// Peut être changer de script
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +22,48 @@ public class GestionAvatar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Si l'avatar n'est pas mort...
-        if (!avatarEstMort)
+        // Si le jeu est en cours...
+        if (partieEnCours)
         {
-            // Si le joueur appuie sur la touche D ou la flèche Droite...
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            // Si l'avatar n'est pas mort...
+            if (!avatarEstMort)
             {
+                ///////////////////////////////////////////////////////////////////////////////////////////////////////////////// DÉPLACEMENTS ET MOUVEMENTS
+                /**************************************************************************************************************** Déplacements horizontaux */
+                // Si le joueur appuie sur la touche D ou la flèche Droite...
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    vitesseX = vitesseXMax; // L'avatar se déplace vers la droite
+                    GetComponent<SpriteRenderer>().flipX = false; // L'avatar regarde vers la droite
+                }
+                // Si le joueur appuie sur la touche A ou la flèche Gauche...
+                else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    vitesseX = -vitesseXMax; // L'avatar se déplace vers la gauche
+                    GetComponent<SpriteRenderer>().flipX = true; // L'avatar regarde vers la gauche
+                }
+                // Mémorise la vitesse actuelle en X
+                else
+                {
+                    vitesseX = GetComponent<Rigidbody2D>().velocity.x;
+                }
 
+                // Applique les vitesses en X et en Y
+                GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
+
+                /**************************************************************************************************************** Animations de déplacement et de repos */
+                // Si la vitesse de l'avatar est supérieur ou inférieur à 0...
+                if (vitesseX > 0.01f || vitesseX < -0.01f)
+                {
+                    GetComponent<Animator>().SetBool("Course", true); // L'animation de course joue
+                }
+                // Sinon...
+                else
+                {
+                    GetComponent<Animator>().SetBool("Course", false); // L'animation de repos joue
+                }
             }
         }
+        
     }
 }
