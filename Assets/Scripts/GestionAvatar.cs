325a813c94e8197789a84c13b65fbcfffeaec443
Taïsha Dorval-Mbele <xshujo@gmail.com>
2023-03-03 15:14:41 -0500

    Début gestion mort et checkpoints

--- Assets/Scripts/GestionAvatar.cs
+++ Assets/Scripts/GestionAvatar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GestionAvatar : MonoBehaviour
 {
@@ -10,25 +11,30 @@ public class GestionAvatar : MonoBehaviour
     float vitesseY; // Variable de type float qui enregistre la vitesse VERTICALE de l'avatar
     public float vitesseSaut; // Variable qui enregistre la vitesse de SAUT de l'avatar
 
-    public static bool avatarEstMort; // Variable statique booléenne qui enregistre si l'avatar est mort
-    public static bool partieEnCours; // Variable statique booléenne qui enregistre si la partie est en cours //////////////////// Peut être changer de script
+    public static Vector2 SpawnPoint; // Variable statique de type vector2 qui enregistre le dernier checkpoint passé par l'avatar
+
+    [Header("Gestion du jeu")]
+    public static bool AvatarEstMort; // Variable statique booléenne qui enregistre si l'avatar est mort
+    public static bool PartieEnCours; // Variable statique booléenne qui enregistre si la partie est en cours //////////////////// Peut être changer de script
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    /* Fin Start */
 
     // Update is called once per frame
     void Update()
     {
         // Si le jeu est en cours...
-        if (partieEnCours)
+        if (PartieEnCours)
         {
             // Si l'avatar n'est pas mort...
-            if (!avatarEstMort)
+            if (!AvatarEstMort)
             {
                 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////// DÉPLACEMENTS ET MOUVEMENTS
+
                 /**************************************************************************************************************** Déplacements horizontaux */
                 // Si le joueur appuie sur la touche D ou la flèche Droite...
                 if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
@@ -77,7 +83,44 @@ public class GestionAvatar : MonoBehaviour
                 // Applique les vitesses en X et en Y
                 GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
             }
+            // Si l'avatar meurt...
+            else
+            {
+                GetComponent<Animator>().SetBool("Mort", true); // L'animation de mort joue
+                Invoke("recommencerPartie", 5f); // On recommence la partie après 5 secondes //////////////////////////////////////// Ajuster délai
+            }
+            /* Fin if (!AvatarEstMort) */
         }
-        
+        /* Fin if (PartieEnCours) */
     }
+    /* Fin Update */
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// GESTION DES ENTRÉES DANS LES ZONES TRIGGER
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Si l'avatar touche un objet dangereux... ///////////////////////////////// OnCollisionEnter2D ?
+        if (collision.gameObject.tag == "Danger")
+        {
+            // Il meurt
+            AvatarEstMort = true;
+        }
+        // Si l'avatar atteint un checkpoint...
+        else if (collision.gameObject.tag == "Checkpoint")
+        {
+            SpawnPoint = collision.transform.position; ////////////////////////////////////////////// À tester
+        }
+    }
+    /* Fin OnTriggerEnter2D() */
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// GESTION DES SCÈNES
+
+    // Fonction qui rejoue la partie quand l'avatar meurt
+    void recommencerPartie()
+    {
+        SceneManager.LoadScene("SceneJeu"); // La partie est relancée
+        transform.position = SpawnPoint; // L'avatar reapparaît au dernier checkpoint sauvegardé
+    }
+    /* Fin recommencerPartie() */
 }
+/* Fin class Mono behaviour */
